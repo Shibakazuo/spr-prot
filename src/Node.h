2124a4ceef85a1461fb6a18e3a5df69a0a244cff
shibakazuo <shibakazuo113@gmail.com>
2018-12-17 16:10:04 +0900

    add graph class

--- src/Node.h
+++ src/Node.h
@@ -17,15 +17,17 @@ public:
 	virtual Environment getEnv() = 0;
 	virtual void setEnv(int x, int y, int z) = 0;
 	virtual void setNext(Node* nextNode) = 0;
+	virtual void setLabel(int label) = 0;
 };
 
 class AssignNode : public Node {
 public:
 	AssignNode();
+	virtual ~AssignNode() { }
 	virtual Environment getEnv();
 	virtual void setEnv(int x, int y, int z);
-	virtual ~AssignNode() { }
 	virtual void setNext(Node* nextNode);
+	virtual void setLabel(int label);
 private:
 	Environment env;
 	int label;
@@ -35,14 +37,16 @@ private:
 class BranchNode : public Node {
 public: 
 	BranchNode(); 
+	virtual ~BranchNode() { }
 	virtual Environment getEnv();
 	virtual void setEnv(int x, int y, int z);
-	virtual ~BranchNode() { }
 	virtual void setNext(Node* nextNode);
+	virtual void setLabel(int label);
 private:
 	Environment env;
 	int label;
-	Node* next;
+	Node* trueNode;
+	Node* falseNode;
 };
 
 
