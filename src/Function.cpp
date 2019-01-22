99f004fc350dae2806a6751f38771eb00b0c4212
shibakazuo <shibakazuo113@gmail.com>
2019-01-22 16:45:54 +0900

    add files

---
+++ src/Function.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <list>
+#include <Function.h>
+
+using namespace std;
+
+void Function::pntError() {
+	cout << "error : there is some problem in variable passing" << endl;
+	exit(1);
+}
