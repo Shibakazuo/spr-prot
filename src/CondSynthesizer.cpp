ced7ad73b117fc82ecd8dbcf095f32533e6dd061
shibakazuo <shibakazuo113@gmail.com>
2019-02-11 19:08:26 +0900

    add synthesizer

---
+++ src/CondSynthesizer.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "CondSynthesizer.h"
+
+using namespace std;
+
+CondSynthesizer::condSynthesize(Graph* target, ProgState* progStat) {
+	
+}
---
+++ src/CondSynthesizer.h
@@ -0,0 +1,14 @@
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Graph.h"
+
+using namespace std;
+
+class CondSynthesizer {
+public:
+	void condSynthesize(Graph* target, ProgState* progStat);
+private:
+
+};
